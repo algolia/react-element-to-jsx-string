742c4edd914340bc0125d2bcfa2089a026b8bb09
Patrick Sachs <kontakt@patrick-sachs.de>
2018-09-05 21:16:16 +0200

    fix(formatter): Precommit script was breaking the tests

--- src/formatter/formatFunction.spec.js
+++ src/formatter/formatFunction.spec.js
@@ -6,78 +6,55 @@ jest.mock('./formatReactElementNode.js', () => node =>
   `<${node.displayName} />`
 );
 
+function hello() {
+  return 1;
+}
+
 describe('formatFunction', () => {
   it('should replace a function with noRefCheck without showFunctions option', () => {
-    expect(
-      formatFunction(function hello() {
-        return 1;
-      }, {})
-    ).toEqual('function noRefCheck() {}');
+    expect(formatFunction(hello, {})).toEqual('function noRefCheck() {}');
   });
 
   it('should replace a function with noRefCheck if showFunctions is false', () => {
-    expect(
-      formatFunction(
-        function hello() {
-          return 1;
-        },
-        { showFunctions: false }
-      )
-    ).toEqual('function noRefCheck() {}');
+    expect(formatFunction(hello, { showFunctions: false })).toEqual(
+      'function noRefCheck() {}'
+    );
   });
 
   it('should format a function if showFunctions is true', () => {
-    expect(
-      formatFunction(
-        function hello() {
-          return 1;
-        },
-        { showFunctions: true }
-      )
-    ).toEqual('function hello() {return 1;}');
+    expect(formatFunction(hello, { showFunctions: true }))
+      .toEqual(`function hello() {
+  return 1;
+}`);
   });
 
   it('should format a function without name if showFunctions is true', () => {
-    expect(
-      formatFunction(
-        function() {
-          return 1;
-        },
-        { showFunctions: true }
-      )
-    ).toEqual('function () {return 1;}');
+    expect(formatFunction(() => 1, { showFunctions: true })).toEqual(
+      'function () {return 1;}'
+    );
   });
 
   it('should use the functionValue option', () => {
-    expect(
-      formatFunction(
-        function hello() {
-          return 1;
-        },
-        { functionValue: () => '<Test />' }
-      )
-    ).toEqual('<Test />');
+    expect(formatFunction(hello, { functionValue: () => '<Test />' })).toEqual(
+      '<Test />'
+    );
   });
 
   it('should use the functionValue option even if showFunctions is true', () => {
     expect(
-      formatFunction(
-        function hello() {
-          return 1;
-        },
-        { showFunctions: true, functionValue: () => '<Test />' }
-      )
+      formatFunction(hello, {
+        showFunctions: true,
+        functionValue: () => '<Test />',
+      })
     ).toEqual('<Test />');
   });
 
   it('should use the functionValue option even if showFunctions is false', () => {
     expect(
-      formatFunction(
-        function hello() {
-          return 1;
-        },
-        { showFunctions: false, functionValue: () => '<Test />' }
-      )
+      formatFunction(hello, {
+        showFunctions: false,
+        functionValue: () => '<Test />',
+      })
     ).toEqual('<Test />');
   });
 });
