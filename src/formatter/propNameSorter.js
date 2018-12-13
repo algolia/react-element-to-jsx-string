a44eaa74e44dbc20435c0fe274eadeae5eff7b1c
Brian Schlenker <bschlenk@umich.edu>
2018-12-13 18:10:30 -0500

    fix: stabilize propNameSorter function

--- src/formatter/propNameSorter.js
+++ src/formatter/propNameSorter.js
@@ -1,13 +1,20 @@
 /* @flow */
 
+const SORT_FIRST = ['key', 'ref'];
+
 export default (sortProps: boolean) => (a: string, b: string): -1 | 0 | 1 => {
   if (a === b) {
     return 0;
   }
 
-  if (['key', 'ref'].includes(a)) {
+  if (SORT_FIRST.includes(a)) {
+    if (SORT_FIRST.includes(b)) {
+      return SORT_FIRST.indexOf(a) - SORT_FIRST.indexOf(b);
+    }
     return -1;
-  } else if (['key', 'ref'].includes(b)) {
+  }
+
+  if (SORT_FIRST.includes(b)) {
     return 1;
   }
 
