53b89ceb8c1b13e48b3eca398b93f5cdaa75e2ab
Armand Abric <armand@forgebinaire.net>
2021-10-02 17:04:09 +0200

    Preserve line break

--- src/formatter/sortPropsByNames.ts
+++ src/formatter/sortPropsByNames.ts
@@ -4,7 +4,9 @@ export default (shouldSortUserProps: boolean) =>
   (props: string[]): string[] => {
     const haveKeyProp = props.includes('key');
     const haveRefProp = props.includes('ref');
+
     const userPropsOnly = props.filter((oneProp) => !isKeyOrRefProps(oneProp));
+
     const sortedProps = shouldSortUserProps
       ? [...userPropsOnly.sort()] // We use basic lexical order
       : [...userPropsOnly];
