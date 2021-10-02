d3743ad10a16444af42c5fde2ef8be659d94a4b4
Armand Abric <armand@forgebinaire.net>
2021-10-02 10:00:13 +0200

    Just run flow-to-ts command

---
+++ src/formatter/createPropFilter.ts
@@ -0,0 +1,10 @@
+export default function createPropFilter(
+  props: {},
+  filter: string[] | ((propValue: any, key: string) => boolean)
+) {
+  if (Array.isArray(filter)) {
+    return (key: string) => filter.indexOf(key) === -1;
+  } else {
+    return (key: string) => filter(props[key], key);
+  }
+}
