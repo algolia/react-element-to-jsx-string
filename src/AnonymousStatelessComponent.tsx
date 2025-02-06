79f4ba6170726be3313c4557d2fe37996af27d17
Armand Abric <armand@forgebinaire.net>
2025-02-06 08:52:56 +0100

    Less than 100 TS errors !

--- src/AnonymousStatelessComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// eslint-disable-next-line func-names
-export default function (props: { children: React.ReactNode }) {
-  const { children } = props; // eslint-disable-line react/prop-types
-
-  return <div>{children}</div>;
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: false,
     setupFiles: ['tests/setupTests.ts'],
