74b094184c804340ecaa10af36e55bc2c69d453f
Valentin Semirulnik <v7rulnik@gmail.com>
2025-04-25 01:01:28 +0300

    split test in to two

--- src/formatter/sortObject.spec.js
+++ src/formatter/sortObject.spec.js
@@ -44,23 +44,48 @@ describe('sortObject', () => {
     });
   });
 
-  it('should remove _owner key from react elements', () => {
-    const fixture = {
-      _owner: "_owner that doesn't belong to react element",
-      component: <div />,
-    };
-
-    expect(JSON.stringify(sortObject(fixture))).toEqual(
-      JSON.stringify({
+  describe('_owner key', () => {
+    it('should preserve the _owner key for objects that are not react elements', () => {
+      const fixture = {
         _owner: "_owner that doesn't belong to react element",
-        component: {
-          $$typeof: Symbol('react.transitional.element'),
-          type: 'div',
-          key: null,
-          props: {},
-          _store: {},
-        },
-      })
-    );
+        foo: 'bar',
+      };
+
+      expect(JSON.stringify(sortObject(fixture))).toEqual(
+        JSON.stringify({
+          _owner: "_owner that doesn't belong to react element",
+          foo: 'bar',
+        })
+      );
+    });
+
+    it('should remove the _owner key from top level react element', () => {
+      const fixture = {
+        reactElement: (
+          <div>
+            <span></span>
+          </div>
+        ),
+      };
+
+      expect(JSON.stringify(sortObject(fixture))).toEqual(
+        JSON.stringify({
+          reactElement: {
+            type: 'div',
+            key: null,
+            props: {
+              children: {
+                type: 'span',
+                key: null,
+                props: {},
+                _owner: null,
+                _store: {},
+              },
+            },
+            _store: {},
+          },
+        })
+      );
+    });
   });
 });
